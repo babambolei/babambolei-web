ea7c7bb2526585c12e06b160fa2e63ac847c754f
Alexandre Masselot <alexandre.masselot@isb-sib.ch>
2015-08-07 20:23:10 +0200

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name babamboleiWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the babamboleiWebApp
+ */
+angular.module('babamboleiWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
